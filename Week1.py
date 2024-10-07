b89ac6637af6136d1d054dc02fc3fcf79978f9e1
Misskitkat <kboscombe@student.bridgew.edu>
2024-10-07 09:40:11 -0400

    Edited week 1 2 3 and added 4 with a very few examples right now

--- Week1.py
+++ Week1.py
@@ -1,7 +1,9 @@
 """
 This is a summary of what we did in week 1. Examples from the thinking tasks as well as others from the book.
 I will put print statements in sections so that when you run the code it will tell you what section you are in.
-
+Try to predict what is going on mentally.
+< click on the number to create a break point where you can run the code until that point.
+In the console there is a little step over button if you have one or multiple break points.
 """
 import random
 
